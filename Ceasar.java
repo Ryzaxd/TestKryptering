16569cf062ffcf609696f4265b2311a3f1c2dd0d
Ryzaxd <nick986h@edu.zealand.dk>
2023-10-05 13:53:49 +0200

    bruteforce metode lavet til begge krypteringsmetoder.

--- Ceasar.java
+++ Ceasar.java
@@ -5,6 +5,7 @@ public class Ceasar {
         int shift = 3;
         String ciphertext = encrypt(plaintext, shift);
         System.out.println(ciphertext); // prints "KHOOR ZRUOG"
+        Ceasar.bruteForce(ciphertext);
     }
 
     public static String encrypt(String plaintext, int shift) {
@@ -21,6 +22,28 @@ public class Ceasar {
         }
         return ciphertext.toString();
     }
-}
 
+    public static void bruteForce(String ciphertext) {
+        for (int shift = 0; shift < 26; shift++) {
+            String plaintext = decrypt(ciphertext, shift);
+            System.out.println("Shift " + shift + ": " + plaintext);
+        }
+    }
+    
+    public static String decrypt(String ciphertext, int shift) {
+        StringBuilder plaintext = new StringBuilder();
+        for (int i = 0; i < ciphertext.length(); i++) {
+            char c = ciphertext.charAt(i);
+            if (Character.isLetter(c)) {
+                c = (char) (c - shift);
+                if (!Character.isLetter(c)) {
+                    c += 26;
+                }
+            }
+            plaintext.append(c);
+        }
+        return plaintext.toString();
+    }
+    
+}
 
--- Viginiere.java
+++ Viginiere.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Viginiere {
     public static void main(String[] args) {
@@ -5,6 +7,7 @@ public class Viginiere {
         String key = "KEY";
         String ciphertext = encrypt(plaintext, key);
         System.out.println(ciphertext); // prints "RIJVS"
+        bruteForce(ciphertext);
     }
 
     public static String encrypt(String plaintext, String key) {
@@ -26,6 +29,53 @@ public class Viginiere {
         }
     return ciphertext;
     }
+
+    public static void bruteForce(String ciphertext) {
+    for (int keyLength = 1; keyLength <= ciphertext.length(); keyLength++) {
+        for (String key : generateKeys(keyLength)) {
+            String plaintext = decrypt(ciphertext, key);
+            System.out.println("Key " + key + ": " + plaintext);
+        }
+    }
+}
+
+public static List<String> generateKeys(int length) {
+    List<String> keys = new ArrayList<>();
+    char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    generateKeysHelper("", alphabet, length, keys);
+    return keys;
+}
+
+private static void generateKeysHelper(String prefix, char[] alphabet, int length, List<String> keys) {
+    if (prefix.length() == length) {
+        keys.add(prefix);
+    } else {
+        for (int i = 0; i < alphabet.length; i++) {
+            generateKeysHelper(prefix + alphabet[i], alphabet, length, keys);
+        }
+    }
+}
+
+public static String decrypt(String ciphertext, String key) {
+    String plaintext = "";
+    int keyIndex = 0;
+    for (int i = 0; i < ciphertext.length(); i++) {
+        char cipherChar = ciphertext.charAt(i);
+        char keyChar = key.charAt(keyIndex);
+        int shift = (int) keyChar - 65;
+        char plainChar = (char) ((int) cipherChar - shift);
+        if (plainChar < 'A') {
+            plainChar = (char) ((int) plainChar + 26);
+        }
+        plaintext += plainChar;
+        keyIndex++;
+        if (keyIndex == key.length()) {
+            keyIndex = 0;
+        }
+    }
+    return plaintext;
+}
+
 }
 
 
